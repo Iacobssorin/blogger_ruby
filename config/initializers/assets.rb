68e20a6494438bf2c2e37af5ce12294db676b59d
iacobssorin <iacobs205@gmail.com>
2019-05-30 13:35:25 +0200

    config.assets.initialize_on_precompile = false

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+config.assets.initialize_on_precompile = false# Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
